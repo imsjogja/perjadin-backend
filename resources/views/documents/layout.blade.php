9dd3bb282b229b0122d89b9d4b6aafa0ad706314
kusniawan <kusniawan.sisteminformasi@gmail.com>
2026-08-25 21:35:17 +0700

    Cegah teks kop turun baris

--- resources/views/documents/layout.blade.php
+++ resources/views/documents/layout.blade.php
@@ -71,7 +71,8 @@
         .kop-logo { padding: 0; text-align: center; vertical-align: top; width: 150px; }
         .kop-logo img { display: block; height: 82px; margin: -7px auto 0; object-fit: contain; width: 58px; }
         .kop-content { padding: 0; position: relative; top: -3px; vertical-align: top; }
-        .kop-spacer { padding: 0; width: 150px; }
+        .kop-content-inner { position: relative; }
+        .kop-with-logo .kop-content-inner { left: -75px; }
         .kop-government { font-family: Times, "Times New Roman", serif; font-size: 19pt; font-weight: normal; letter-spacing: 0; line-height: .9; margin: 0 0 -4px; text-transform: uppercase; }
         .kop-agency { font-family: "Source Sans 3", "Source Sans Pro", sans-serif; font-size: 32px; font-weight: 900; letter-spacing: .02rem; line-height: .9; margin: 0; text-transform: uppercase; }
         @if(! empty($stationery['arial_black_font_path']) && is_readable($stationery['arial_black_font_path']))
--- resources/views/documents/partials/stationery.blade.php
+++ resources/views/documents/partials/stationery.blade.php
@@ -8,13 +8,12 @@
             <td class="kop-logo"><img src="{{ $stationery['logo_data_uri'] }}" alt="Logo instansi"></td>
         @endif
         <td class="kop-content">
-            <h2 class="kop-government">Pemerintah {{ $stationery['government'] }}</h2>
-            <h2 class="kop-agency">{{ $useSecretariat ? $stationery['secretariat'] : $stationery['agency'] }}</h2>
-            <span class="kop-address">{{ $stationery['address'] }} - {{ $stationery['city'] }}</span>
+            <div class="kop-content-inner">
+                <h2 class="kop-government">Pemerintah {{ $stationery['government'] }}</h2>
+                <h2 class="kop-agency">{{ $useSecretariat ? $stationery['secretariat'] : $stationery['agency'] }}</h2>
+                <span class="kop-address">{{ $stationery['address'] }} - {{ $stationery['city'] }}</span>
+            </div>
         </td>
-        @if(! empty($stationery['logo_data_uri']))
-            <td class="kop-spacer"></td>
-        @endif
     </tr>
-    <tr><td class="kop-line" colspan="{{ ! empty($stationery['logo_data_uri']) ? 3 : 1 }}"></td></tr>
+    <tr><td class="kop-line" colspan="{{ ! empty($stationery['logo_data_uri']) ? 2 : 1 }}"></td></tr>
 </table>
